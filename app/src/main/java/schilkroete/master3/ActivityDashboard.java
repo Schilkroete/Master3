7b895b79ca733bef8a7103c528e51bef2cd27f98
Schilkroete <schilkroete@gmail.com>
2017-02-02 12:12:22 +0100

    - Bilder in drawable hinzugefügt - Layout für das Dashboard erstellt und Namen geändert - In ActivityDashboard.java neue Funkktion eingebaut (kleine Spielerei)

--- app/src/main/java/schilkroete/master3/ActivityDashboard.java
+++ app/src/main/java/schilkroete/master3/ActivityDashboard.java
@@ -2,12 +2,35 @@ package schilkroete.master3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ActivityDashboard extends Activity {
+
+    ImageButton btn;
+    boolean istGeklickt = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.layout_dashboard);
+
+
+        // TODO Dies ist ein Test
+        btn = (ImageButton) findViewById(R.id.btnSettings);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (istGeklickt) {
+                    btn.setImageResource(R.drawable.change);
+                } else {
+                    btn.setImageResource(R.drawable.settings);
+                }
+                istGeklickt =! istGeklickt;
+            }
+        });
+
     }
+
+
 }
