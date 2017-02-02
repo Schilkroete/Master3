f15b415ee29a41aa52762a085b2bdc9c203ce575
Schilkroete <schilkroete@gmail.com>
2017-02-02 11:50:07 +0100

    NewProject - no changes

---
+++ app/src/main/java/schilkroete/master3/ActivityDashboard.java
@@ -0,0 +1,13 @@
+package schilkroete.master3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityDashboard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
